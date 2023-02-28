4e3955e7504784641182e258b8855ca47e3e73e5
Bernardo Nunes <bmn02@outlook.com>
2023-02-28 17:58:53 -0300

    feat: post hard coded and your map

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,48 @@ import { Header } from "./components/Header";
 import { Post } from "./components/Post";
 
 import styles from "./App.module.css";
-import "./global.css";
 import { Sidebar } from "./components/Sidebar";
+
+import "./global.css";
+
+const post = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/bernardomnunes.png",
+      name: "Bernardo Nunes",
+      role: "Front-end Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fizno NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2023-02-25 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "Front-end Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fizno NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2023-02-20 20:00:00"),
+  },
+];
 
 export function App() {
   return (
@@ -13,10 +53,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Bernardo"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem voluptates porro at dolorum dignissimos totam nostrum eius quas ullam, eaque perferendis alias corrupti expedita ea maiores consequuntur officiis illum voluptatibus."
-          />
+          {post.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
